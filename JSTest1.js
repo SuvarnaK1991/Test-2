67d47a59ce29a55cfac973ed7b8837340bb5e80a
SuvarnaK1991 <suvarna.achu@gmail.com>
2021-08-01 17:37:53 +0530

    first commit

---
+++ JSTest1.js
@@ -0,0 +1,7 @@
+<html>
+<head>This is a sample JS</head>
+<body>
+<span id="para1">Change me by clicking on the button</span>
+<button type="button" onclick="document.getElementById('para1').innerHTML='I am changed!!!'"></button>
+</body>
+</html>
